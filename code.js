563f67b85ef0c9fa34691f616983b54d8d27fd12
zackrdavis <zackrdavis@gmail.com>
2014-01-02 10:49:29 -0500

    finalized new portfolio items, new header, etc

--- code.js
+++ code.js
@@ -16,6 +16,32 @@ $(document).ready(function(){
          scrollLeft: $(this).offset().left
       }, 2000);
    });
+
+   $(".expand").click(function(e){
+      if($(e.target).hasClass('noshrink')){
+         
+      } else {
+         var current_div = $(this);
+         $(this).toggleClass('shrunk expanded');
+         $(".expanded").not(current_div).toggleClass('shrunk expanded');
+      }
+   });    
+   
+   $(".expand").click(function(){
+      var item = $(this).attr('id');
+      // console.log(item);
+      var itemIndex = $(this).index('.expand');
+      var shrunkWidth = 260;
+      var itemPos = itemIndex * (shrunkWidth + 63) + 75; 
+      
+      if ($(this).hasClass('expanded')) {
+         var centerOffset = (($(window).width())/2)-(908/2);
+      } else {
+         var centerOffset = (($(window).width())/2)-(shrunkWidth/2);
+      };
+      
+      $("#content").animate({scrollLeft:(itemPos - centerOffset)}, 50
+   });
    
 });
 
